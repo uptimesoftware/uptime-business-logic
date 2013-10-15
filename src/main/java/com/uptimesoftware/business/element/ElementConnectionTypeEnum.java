03c08a6c6ba5c4b41b6f0959832c6569d3ededec
Jonathan Albrecht <jonathan.albrecht@uptimesoftware.com>
2013-10-15 20:19:37 +0000

    UTS-1533 Add an enum for add element connection type.
    git-svn-id: svn://uptime-source/uptime-common/uptime-business-logic/trunk@38122 b0f7ca3f-2a71-40c0-9be3-62e7aa9afdaf

---
+++ src/main/java/com/uptimesoftware/business/element/ElementConnectionTypeEnum.java
@@ -0,0 +1,34 @@
+package com.uptimesoftware.business.element;
+
+import java.util.Map;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableMap.Builder;
+
+public enum ElementConnectionTypeEnum {
+	Agent("agent");
+
+	private static final Map<String, ElementConnectionTypeEnum> JSON_NAMES_MAP;
+
+	static {
+		Builder<String, ElementConnectionTypeEnum> builder = ImmutableMap.builder();
+		for (ElementConnectionTypeEnum value : ElementConnectionTypeEnum.values()) {
+			builder.put(value.getJsonName(), value);
+		}
+		JSON_NAMES_MAP = builder.build();
+	}
+
+	private String jsonName;
+
+	private ElementConnectionTypeEnum(String jsonName) {
+		this.jsonName = jsonName;
+	}
+
+	public String getJsonName() {
+		return jsonName;
+	}
+
+	public static ElementConnectionTypeEnum fromJsonName(String jsonName) {
+		return JSON_NAMES_MAP.get(jsonName);
+	}
+}
