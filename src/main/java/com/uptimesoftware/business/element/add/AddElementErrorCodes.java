b3aebf0425d597c1de251238952c37ceb7890b5c
Jonathan Albrecht <jonathan.albrecht@uptimesoftware.com>
2013-10-18 18:13:40 +0000

    UTS-1533 Add a duplicate hostname error code.
    git-svn-id: svn://uptime-source/uptime-common/uptime-business-logic/trunk@38158 b0f7ca3f-2a71-40c0-9be3-62e7aa9afdaf

--- src/main/java/com/uptimesoftware/business/element/add/AddElementErrorCodes.java
+++ src/main/java/com/uptimesoftware/business/element/add/AddElementErrorCodes.java
@@ -4,4 +4,5 @@ public class AddElementErrorCodes {
 	public static final String MISSING_FIELD = "UT-2000";
 	public static final String NUMBER_OUT_OF_RANGE = "UT-2001";
 	public static final String TOO_LONG = "UT-2002";
+	public static final String DUPLICATE_HOSTNAME = "UT-2003";
 }
