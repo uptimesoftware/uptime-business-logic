97f18980a319c1023e510d68a1f99cfee25cd21d
Jonathan Albrecht <jonathan.albrecht@uptimesoftware.com>
2013-10-22 19:23:51 +0000

    UTS-1533 Add a no visible element groups error code.
    git-svn-id: svn://uptime-source/uptime-common/uptime-business-logic/trunk@38189 b0f7ca3f-2a71-40c0-9be3-62e7aa9afdaf

--- src/main/java/com/uptimesoftware/business/element/add/AddElementErrorCodes.java
+++ src/main/java/com/uptimesoftware/business/element/add/AddElementErrorCodes.java
@@ -5,4 +5,5 @@ public class AddElementErrorCodes {
 	public static final String NUMBER_OUT_OF_RANGE = "UT-2001";
 	public static final String TOO_LONG = "UT-2002";
 	public static final String DUPLICATE_HOSTNAME = "UT-2003";
+	public static final String NO_VISIBLE_ELEMENT_GROUPS = "UT-2004";
 }
