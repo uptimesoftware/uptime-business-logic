8276daa8f62fc3c0001d66ba1e48ab44c6e88a5d
Jonathan Albrecht <jonathan.albrecht@uptimesoftware.com>
2013-10-22 16:34:03 +0000

    UTS-1533 Now that we are returning Booleans, the javascript when script must return convert to a boolean so that the ! operator works as expected.
    git-svn-id: svn://uptime-source/uptime-common/uptime-business-logic/trunk@38181 b0f7ca3f-2a71-40c0-9be3-62e7aa9afdaf

--- src/main/java/com/uptimesoftware/business/element/add/AddAgentElementCollectionMethod.java
+++ src/main/java/com/uptimesoftware/business/element/add/AddAgentElementCollectionMethod.java
@@ -8,10 +8,10 @@ import com.uptimesoftware.business.element.ElementConnectionTypeEnum;
 public class AddAgentElementCollectionMethod implements ElementCollectionMethod {
 
 	private final Boolean useGlobalConnectionSettings;
-	@NotNull(message = "The agent port is required", errorCode = AddElementErrorCodes.MISSING_FIELD, when = "jrejs:!_this.isUseGlobalConnectionSettings()")
-	@Range(min = 1, max = 65535, message = "The agent port must be a number between {min} and {max}", errorCode = AddElementErrorCodes.NUMBER_OUT_OF_RANGE, when = "jrejs:!_this.isUseGlobalConnectionSettings()")
+	@NotNull(message = "The agent port is required", errorCode = AddElementErrorCodes.MISSING_FIELD, when = "jrejs:!_this.isUseGlobalConnectionSettings().booleanValue()")
+	@Range(min = 1, max = 65535, message = "The agent port must be a number between {min} and {max}", errorCode = AddElementErrorCodes.NUMBER_OUT_OF_RANGE, when = "jrejs:!_this.isUseGlobalConnectionSettings().booleanValue()")
 	private final Integer port;
-	@NotNull(message = "The agent use SSL value is required", errorCode = AddElementErrorCodes.MISSING_FIELD, when = "jrejs:!_this.isUseGlobalConnectionSettings()")
+	@NotNull(message = "The agent use SSL value is required", errorCode = AddElementErrorCodes.MISSING_FIELD, when = "jrejs:!_this.isUseGlobalConnectionSettings().booleanValue()")
 	private final Boolean useSSL;
 
 	public AddAgentElementCollectionMethod(Boolean useGlobalConnectionSettings, Integer port, Boolean useSsl) {
