08a7a8215a2f945a7a197d7d1bb429c9716ee9f1
Jonathan Albrecht <jonathan.albrecht@uptimesoftware.com>
2013-11-08 20:30:08 +0000

    UTS-1534 Add oval validation for the connectionType.
    git-svn-id: svn://uptime-source/uptime-common/uptime-business-logic/trunk@38295 b0f7ca3f-2a71-40c0-9be3-62e7aa9afdaf

--- src/main/java/com/uptimesoftware/business/element/add/NullElementConnectionMethod.java
+++ src/main/java/com/uptimesoftware/business/element/add/NullElementConnectionMethod.java
@@ -1,15 +1,33 @@
 package com.uptimesoftware.business.element.add;
 
+import net.sf.oval.constraint.NotNull;
+
 import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 
 import com.uptimesoftware.business.element.ElementConnectionTypeEnum;
 
+/**
+ * This class is so that json parsing won't fail out if we get an unknown connectionType. We wan't parsing to continue so that we
+ * can return proper validation messages.
+ * 
+ */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class NullElementConnectionMethod implements ElementCollectionMethod {
+
+	@NotNull(message = "A valid connectionType value is required", errorCode = AddElementErrorCodes.MISSING_FIELD)
+	private String connectionType;
 
 	@Override
 	public ElementConnectionTypeEnum getConnectionType() {
 		return null;
+	}
+
+	public String getConnectionTypeString() {
+		return connectionType;
+	}
+
+	public void setConnectionType(String connectionType) {
+		this.connectionType = connectionType;
 	}
 
 }
