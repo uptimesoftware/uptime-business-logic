2bee6d0f43c6fd8d74eef77609224ac28109591a
Jonathan Albrecht <jonathan.albrecht@uptimesoftware.com>
2013-11-15 21:54:44 +0000

    UTS-1534 Perform duplicate hostname and display name checks up front in the controller.
    git-svn-id: svn://uptime-source/uptime-common/uptime-business-logic/trunk@38371 b0f7ca3f-2a71-40c0-9be3-62e7aa9afdaf

--- src/main/java/com/uptimesoftware/business/element/ElementBodyErrors.java
+++ src/main/java/com/uptimesoftware/business/element/ElementBodyErrors.java
@@ -1,5 +1,6 @@
 package com.uptimesoftware.business.element;
 
+import java.util.Iterator;
 import java.util.List;
 
 import javax.annotation.Nullable;
@@ -7,26 +8,52 @@ import javax.annotation.Nullable;
 import net.sf.oval.ConstraintViolation;
 import net.sf.oval.context.FieldContext;
 
+import org.codehaus.jackson.annotate.JsonIgnore;
+
 import com.google.common.base.Function;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
 import com.uptimesoftware.business.validation.oval.ConstraintViolations;
 
-public class ElementBodyErrors {
+public class ElementBodyErrors implements Iterable<ElementBodyError> {
 
 	private final List<ElementBodyError> errors;
 
-	public ElementBodyErrors(List<ElementBodyError> errors) {
-		this.errors = errors;
+	public ElementBodyErrors(Iterable<ElementBodyError> errors) {
+		this.errors = Lists.newArrayList(errors);
+	}
+
+	public ElementBodyErrors(ElementBodyError... errors) {
+		this.errors = Lists.newArrayList(errors);
+	}
+
+	public ElementBodyErrors() {
+		this.errors = Lists.newArrayList();
 	}
 
 	public List<ElementBodyError> getErrors() {
-		return errors;
+		return ImmutableList.copyOf(errors);
 	}
 
-	public static ElementBodyErrors create(List<ConstraintViolation> violations) {
-		return new ElementBodyErrors(ImmutableList.<ElementBodyError> builder()
-				.addAll(Iterables.transform(violations, ToAddElementValidationError)).build());
+	public ElementBodyErrors addError(ElementBodyError error) {
+		errors.add(error);
+		return this;
+	}
+
+	public ElementBodyErrors addErrors(Iterable<ElementBodyError> error) {
+		Iterables.addAll(errors, error);
+		return this;
+	}
+
+	public ElementBodyErrors addViolation(ConstraintViolation violation) {
+		errors.add(ToAddElementValidationError.apply(violation));
+		return this;
+	}
+
+	public ElementBodyErrors addViolations(Iterable<ConstraintViolation> violations) {
+		Iterables.addAll(errors, Iterables.transform(violations, ToAddElementValidationError));
+		return this;
 	}
 
 	public static final Function<ConstraintViolation, ElementBodyError> ToAddElementValidationError = new Function<ConstraintViolation, ElementBodyError>() {
@@ -44,5 +71,21 @@ public class ElementBodyErrors {
 			return new ElementBodyError(field, input.getErrorCode(), input.getMessage(), invalidValue);
 		}
 	};
+
+	@Override
+	@JsonIgnore
+	public Iterator<ElementBodyError> iterator() {
+		return errors.iterator();
+	}
+
+	@JsonIgnore
+	public int size() {
+		return errors.size();
+	}
+
+	@JsonIgnore
+	public boolean isEmpty() {
+		return errors.isEmpty();
+	}
 
 }
