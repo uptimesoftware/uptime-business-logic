a091eb21d6500f414c2c2f35b183c2487084dfba
Jonathan Albrecht <jonathan.albrecht@uptimesoftware.com>
2013-11-21 21:45:49 +0000

    UTS-1535 Use the default name as the json value when converting to/from json.
    git-svn-id: svn://uptime-source/uptime-common/uptime-business-logic/trunk@38421 b0f7ca3f-2a71-40c0-9be3-62e7aa9afdaf

--- src/main/java/com/uptimesoftware/business/element/ElementTypeEnum.java
+++ src/main/java/com/uptimesoftware/business/element/ElementTypeEnum.java
@@ -1,9 +1,27 @@
 package com.uptimesoftware.business.element;
 
+import java.util.Map;
+
+import org.codehaus.jackson.annotate.JsonCreator;
+import org.codehaus.jackson.annotate.JsonValue;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableMap.Builder;
+
 public enum ElementTypeEnum {
 	Server("Server"),
 	NetworkDevice("Network Device"),
 	Application("Application");
+
+	private static final Map<String, ElementTypeEnum> JSON_NAMES_MAP;
+
+	static {
+		Builder<String, ElementTypeEnum> builder = ImmutableMap.builder();
+		for (ElementTypeEnum value : ElementTypeEnum.values()) {
+			builder.put(value.getDefaultName(), value);
+		}
+		JSON_NAMES_MAP = builder.build();
+	}
 
 	private final String defaultName;
 
@@ -11,7 +29,13 @@ public enum ElementTypeEnum {
 		this.defaultName = defaultName;
 	}
 
+	@JsonValue
 	public String getDefaultName() {
 		return defaultName;
+	}
+
+	@JsonCreator
+	public static ElementTypeEnum fromJsonName(String jsonName) {
+		return JSON_NAMES_MAP.get(jsonName);
 	}
 }
