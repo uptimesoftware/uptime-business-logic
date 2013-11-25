628e175fae6f85326c13037146ffdfffc43a6178
Jonathan Albrecht <jonathan.albrecht@uptimesoftware.com>
2013-11-25 14:37:15 +0000

    UTS-1534 Try to fix weird compile error on controller build.
    git-svn-id: svn://uptime-source/uptime-common/uptime-business-logic/trunk@38476 b0f7ca3f-2a71-40c0-9be3-62e7aa9afdaf

--- src/main/java/com/uptimesoftware/business/error/MessengerErrors.java
+++ src/main/java/com/uptimesoftware/business/error/MessengerErrors.java
@@ -1,8 +1,6 @@
 package com.uptimesoftware.business.error;
 
-import com.uptimesoftware.util.jsonrpc.JsonRpcResponseErrorCode;
-
 public class MessengerErrors {
-	public static final int MessengerCommunicationErrorCodeInt = JsonRpcResponseErrorCode.MESSENGER_EXCEPTION.getCode();
+	public static final int MessengerCommunicationErrorCodeInt = -1016;
 	public static final String MessengerCommunicationErrorCode = String.format("UT%d", MessengerCommunicationErrorCodeInt);
 }
