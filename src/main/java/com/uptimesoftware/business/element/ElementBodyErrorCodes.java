1345c263ada209883d277968211fa19ca9fd9861
Jonathan Albrecht <jonathan.albrecht@uptimesoftware.com>
2013-12-06 16:52:35 +0000

    UTS-1603 First cut at adding elements to a specific group. The groupId field is now required.
    git-svn-id: svn://uptime-source/uptime-common/uptime-business-logic/trunk@38612 b0f7ca3f-2a71-40c0-9be3-62e7aa9afdaf

--- src/main/java/com/uptimesoftware/business/element/ElementBodyErrorCodes.java
+++ src/main/java/com/uptimesoftware/business/element/ElementBodyErrorCodes.java
@@ -5,7 +5,6 @@ public class ElementBodyErrorCodes {
 	public static final String NUMBER_OUT_OF_RANGE = "UT-2001";
 	public static final String TOO_LONG = "UT-2002";
 	public static final String DUPLICATE_HOSTNAME = "UT-2003";
-	public static final String NO_VISIBLE_ELEMENT_GROUPS = "UT-2004";
 	public static final String LICENSE_VIOLATION = "UT-2005";
 	public static final String SPACES_IN_HOSTNAME = "UT-2006";
 }
--- src/main/java/com/uptimesoftware/business/element/add/AddElementBasicInfo.java
+++ src/main/java/com/uptimesoftware/business/element/add/AddElementBasicInfo.java
@@ -2,8 +2,12 @@ package com.uptimesoftware.business.element.add;
 
 import net.sf.oval.constraint.CheckWith;
 import net.sf.oval.constraint.Length;
+import net.sf.oval.constraint.Min;
 import net.sf.oval.constraint.NotBlank;
 import net.sf.oval.constraint.NotNull;
+
+import org.codehaus.jackson.annotate.JsonCreator;
+import org.codehaus.jackson.annotate.JsonProperty;
 
 import com.uptimesoftware.business.element.ElementBodyErrorCodes;
 import com.uptimesoftware.business.element.ElementTypeEnum;
@@ -25,23 +29,22 @@ public class AddElementBasicInfo {
 	private final String hostname;
 	@NotNull(message = "The element type is required", errorCode = ElementBodyErrorCodes.MISSING_FIELD)
 	private final ElementTypeEnum type;
+	@NotNull(message = "The group id is required", errorCode = ElementBodyErrorCodes.MISSING_FIELD)
+	@Min(value = 1, message = "The group id must be greater than or equal to {invalidValue}", errorCode = ElementBodyErrorCodes.NUMBER_OUT_OF_RANGE)
+	private final Long groupId;
 	@NotNull(message = "The element collection method is required", errorCode = ElementBodyErrorCodes.MISSING_FIELD)
 	@ValidateNestedProperty
 	private final ElementCollectionMethod collectionMethod;
 
-	/**
-	 * for json deserialization
-	 */
-	public AddElementBasicInfo() {
-		this(null, null, null, null, null);
-	}
-
-	public AddElementBasicInfo(String name, String description, String hostname, ElementTypeEnum elementType,
-			ElementCollectionMethod collectionMethod) {
+	@JsonCreator
+	public AddElementBasicInfo(@JsonProperty("name") String name, @JsonProperty("description") String description,
+			@JsonProperty("hostname") String hostname, @JsonProperty("elementType") ElementTypeEnum elementType,
+			@JsonProperty("groupId") Long groupId, @JsonProperty("collectionMethod") ElementCollectionMethod collectionMethod) {
 		this.name = name;
 		this.description = description;
 		this.hostname = hostname;
 		this.type = elementType;
+		this.groupId = groupId;
 		this.collectionMethod = collectionMethod;
 	}
 
@@ -59,6 +62,10 @@ public class AddElementBasicInfo {
 
 	public ElementTypeEnum getType() {
 		return type;
+	}
+
+	public Long getGroupId() {
+		return groupId;
 	}
 
 	public ElementCollectionMethod getCollectionMethod() {
