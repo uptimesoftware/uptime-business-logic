00a3703c63f95c1aa079cca38efe6970c5740e09
Jonathan Albrecht <jonathan.albrecht@uptimesoftware.com>
2013-12-09 15:11:57 +0000

    UTS-1604 Move the My Infrastructure group name and id to business logic because the controller needs it for validation.
    git-svn-id: svn://uptime-source/uptime-common/uptime-business-logic/trunk@38617 b0f7ca3f-2a71-40c0-9be3-62e7aa9afdaf

---
+++ src/main/java/com/uptimesoftware/business/elementgroup/ElementGroups.java
@@ -0,0 +1,7 @@
+package com.uptimesoftware.business.elementgroup;
+
+public class ElementGroups {
+	public final static Long MY_INFRASTRUCTURE_ID = 1L;
+	public final static String MY_INFRASTRUCTURE_NAME = "My Infrastructure";
+
+}
