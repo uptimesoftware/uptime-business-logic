271e2cd38a270a146f3f446f1af173c7689e81df
ushalather <usha.lather@accolitedigital.com>
2023-04-17 12:44:43 +0530

    UT-18865 Log4j 1 dependency removed from business logic. That was optional

--- src/main/java/com/uptimesoftware/business/validation/oval/ValidatorFactory.java
+++ src/main/java/com/uptimesoftware/business/validation/oval/ValidatorFactory.java
@@ -1,12 +1,12 @@
 package com.uptimesoftware.business.validation.oval;
 
 import net.sf.oval.Validator;
-import net.sf.oval.logging.LoggerFactoryL4JImpl;
+import net.sf.oval.logging.LoggerFactorySLF4JImpl;
 
 public class ValidatorFactory {
 
 	static {
-		Validator.setLoggerFactory(new LoggerFactoryL4JImpl());
+		Validator.setLoggerFactory(new LoggerFactorySLF4JImpl());
 	}
 
 	public static Validator getValidator() {
